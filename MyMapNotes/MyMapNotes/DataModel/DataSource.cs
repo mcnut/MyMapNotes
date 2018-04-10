16c26afbb01ca12c3f6a2f9fc4f5a10bdc1b970b
mcnut <G00328962@gmit.ie>
2018-04-10 06:44:12 +0100

    Created DataModel folder and DataSource class

---
+++ MyMapNotes/MyMapNotes/DataModel/DataSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMapNotes.DataModel
+{
+    // Create a MapNote model
+    public class MapNote
+    {
+        // title, note body, date, latitude, longitude
+    }
+
+    class DataSource
+    {
+        // Keeps a collection of notes and allows user to read, write, update and delete notes.
+    }
+}
